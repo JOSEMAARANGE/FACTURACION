86470f13c0f2a8c89ece292a686a4a77359841af
jose arange <jose_vcb87@hotmail.com>
2024-01-10 13:24:06 -0300

    asiento contables

--- FACTURACION/Form1.Designer.cs
+++ FACTURACION/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FACTURACION
 {
-    partial class Form1
+    partial class AsientoContable
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,163 +29,55 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.motivoajusteLabel = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.MotivoAjusteComboBox = new System.Windows.Forms.ComboBox();
+            this.AsientoEnEdicionBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.FiltradofechasLabel = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.FiltradoMotivoslabel = new System.Windows.Forms.Label();
-            this.FiltradoMotivoComboBox = new System.Windows.Forms.ComboBox();
-            this.NotaExplicativaLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.AsientoContableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnGenerarInformeDetallado = new System.Windows.Forms.Button();
             this.Emitirbutton = new System.Windows.Forms.Button();
             this.Salirbutton = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.CreditosyDebitosBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.motivoDelAjusteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.notaExplicativaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.creditoDebitoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.fechaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.EnEdicionBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AsientoEnEdicionBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.CreditosyDebitosBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.EnEdicionBindingSource)).BeginInit();
-            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AsientoContableBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // motivoajusteLabel
+            // AsientoEnEdicionBindingSource
             // 
-            this.motivoajusteLabel.AutoSize = true;
-            this.motivoajusteLabel.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.motivoajusteLabel.Location = new System.Drawing.Point(8, 56);
-            this.motivoajusteLabel.Name = "motivoajusteLabel";
-            this.motivoajusteLabel.Size = new System.Drawing.Size(109, 21);
-            this.motivoajusteLabel.TabIndex = 0;
-            this.motivoajusteLabel.Text = "Motivo del ajuste:";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.EnEdicionBindingSource, "NotaExplicativa", true));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 106);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(377, 156);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            // 
-            // MotivoAjusteComboBox
-            // 
-            this.MotivoAjusteComboBox.AutoCompleteCustomSource.AddRange(new string[] {
-            "Errores en categoría",
-            "Errores en lecturas ",
-            "Error en condición de IVA"});
-            this.MotivoAjusteComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.EnEdicionBindingSource, "MotivoDelAjuste", true));
-            this.MotivoAjusteComboBox.FormattingEnabled = true;
-            this.MotivoAjusteComboBox.Items.AddRange(new object[] {
-            "Errores en categoría",
-            "Errores en lecturas",
-            "Error en condición de IVA"});
-            this.MotivoAjusteComboBox.Location = new System.Drawing.Point(123, 54);
-            this.MotivoAjusteComboBox.Name = "MotivoAjusteComboBox";
-            this.MotivoAjusteComboBox.Size = new System.Drawing.Size(266, 21);
-            this.MotivoAjusteComboBox.TabIndex = 2;
+            this.AsientoEnEdicionBindingSource.DataSource = typeof(ENTIDADES.CreditosDebitos);
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.motivoDelAjusteDataGridViewTextBoxColumn,
-            this.notaExplicativaDataGridViewTextBoxColumn,
-            this.creditoDebitoDataGridViewTextBoxColumn,
-            this.fechaDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.CreditosyDebitosBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(440, 95);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(585, 292);
             this.dataGridView1.TabIndex = 3;
             // 
-            // FiltradofechasLabel
+            // AsientoContableBindingSource
             // 
-            this.FiltradofechasLabel.AutoSize = true;
-            this.FiltradofechasLabel.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FiltradofechasLabel.Location = new System.Drawing.Point(10, 34);
-            this.FiltradofechasLabel.Name = "FiltradofechasLabel";
-            this.FiltradofechasLabel.Size = new System.Drawing.Size(95, 21);
-            this.FiltradofechasLabel.TabIndex = 5;
-            this.FiltradofechasLabel.Text = "Filtrado Fechas:";
+            this.AsientoContableBindingSource.DataSource = typeof(ENTIDADES.CreditosDebitos);
             // 
-            // dateTimePicker1
+            // btnGenerarInformeDetallado
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(136, 19);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(193, 20);
-            this.dateTimePicker1.TabIndex = 6;
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(135, 45);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(194, 20);
-            this.dateTimePicker2.TabIndex = 7;
-            // 
-            // FiltradoMotivoslabel
-            // 
-            this.FiltradoMotivoslabel.AutoSize = true;
-            this.FiltradoMotivoslabel.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FiltradoMotivoslabel.Location = new System.Drawing.Point(6, 16);
-            this.FiltradoMotivoslabel.Name = "FiltradoMotivoslabel";
-            this.FiltradoMotivoslabel.Size = new System.Drawing.Size(125, 21);
-            this.FiltradoMotivoslabel.TabIndex = 8;
-            this.FiltradoMotivoslabel.Text = "Filtrado Por Motivos:";
-            // 
-            // FiltradoMotivoComboBox
-            // 
-            this.FiltradoMotivoComboBox.AutoCompleteCustomSource.AddRange(new string[] {
-            "Errores en categoría",
-            "Errores en lecturas ",
-            "Error en condición de IVA"});
-            this.FiltradoMotivoComboBox.FormattingEnabled = true;
-            this.FiltradoMotivoComboBox.Items.AddRange(new object[] {
-            "Errores en categoría",
-            "Errores en lecturas",
-            "Error en condición de IVA"});
-            this.FiltradoMotivoComboBox.Location = new System.Drawing.Point(132, 14);
-            this.FiltradoMotivoComboBox.Name = "FiltradoMotivoComboBox";
-            this.FiltradoMotivoComboBox.Size = new System.Drawing.Size(199, 21);
-            this.FiltradoMotivoComboBox.TabIndex = 9;
-            // 
-            // NotaExplicativaLabel
-            // 
-            this.NotaExplicativaLabel.AutoSize = true;
-            this.NotaExplicativaLabel.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NotaExplicativaLabel.Location = new System.Drawing.Point(15, 86);
-            this.NotaExplicativaLabel.Name = "NotaExplicativaLabel";
-            this.NotaExplicativaLabel.Size = new System.Drawing.Size(93, 20);
-            this.NotaExplicativaLabel.TabIndex = 10;
-            this.NotaExplicativaLabel.Text = "Nota Explicativa:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(461, 402);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Generar Informe Detallado";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGenerarInformeDetallado.Location = new System.Drawing.Point(461, 402);
+            this.btnGenerarInformeDetallado.Name = "btnGenerarInformeDetallado";
+            this.btnGenerarInformeDetallado.Size = new System.Drawing.Size(167, 23);
+            this.btnGenerarInformeDetallado.TabIndex = 11;
+            this.btnGenerarInformeDetallado.Text = "Generar Informe Detallado";
+            this.btnGenerarInformeDetallado.UseVisualStyleBackColor = true;
             // 
             // Emitirbutton
             // 
             this.Emitirbutton.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.Emitirbutton.Location = new System.Drawing.Point(23, 270);
+            this.Emitirbutton.Location = new System.Drawing.Point(34, 402);
             this.Emitirbutton.Name = "Emitirbutton";
             this.Emitirbutton.Size = new System.Drawing.Size(75, 23);
             this.Emitirbutton.TabIndex = 12;
             this.Emitirbutton.Text = "EMITIR";
             this.Emitirbutton.UseVisualStyleBackColor = false;
+            this.Emitirbutton.Click += new System.EventHandler(this.Emitirbutton_Click);
             // 
             // Salirbutton
             // 
@@ -196,115 +88,34 @@
             this.Salirbutton.TabIndex = 13;
             this.Salirbutton.Text = "SALIR";
             this.Salirbutton.UseVisualStyleBackColor = false;
+            this.Salirbutton.Click += new System.EventHandler(this.Salirbutton_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
-            this.groupBox1.Controls.Add(this.FiltradofechasLabel);
-            this.groupBox1.Location = new System.Drawing.Point(40, 299);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(335, 88);
-            this.groupBox1.TabIndex = 14;
-            this.groupBox1.TabStop = false;
-            // 
-            // CreditosyDebitosBindingSource
-            // 
-            this.CreditosyDebitosBindingSource.DataSource = typeof(ENTIDADES.CreditosDebitos);
-            // 
-            // motivoDelAjusteDataGridViewTextBoxColumn
-            // 
-            this.motivoDelAjusteDataGridViewTextBoxColumn.DataPropertyName = "MotivoDelAjuste";
-            this.motivoDelAjusteDataGridViewTextBoxColumn.HeaderText = "MotivoDelAjuste";
-            this.motivoDelAjusteDataGridViewTextBoxColumn.Name = "motivoDelAjusteDataGridViewTextBoxColumn";
-            // 
-            // notaExplicativaDataGridViewTextBoxColumn
-            // 
-            this.notaExplicativaDataGridViewTextBoxColumn.DataPropertyName = "NotaExplicativa";
-            this.notaExplicativaDataGridViewTextBoxColumn.HeaderText = "NotaExplicativa";
-            this.notaExplicativaDataGridViewTextBoxColumn.Name = "notaExplicativaDataGridViewTextBoxColumn";
-            // 
-            // creditoDebitoDataGridViewTextBoxColumn
-            // 
-            this.creditoDebitoDataGridViewTextBoxColumn.DataPropertyName = "CreditoDebito";
-            this.creditoDebitoDataGridViewTextBoxColumn.HeaderText = "CreditoDebito";
-            this.creditoDebitoDataGridViewTextBoxColumn.Name = "creditoDebitoDataGridViewTextBoxColumn";
-            // 
-            // fechaDataGridViewTextBoxColumn
-            // 
-            this.fechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha";
-            this.fechaDataGridViewTextBoxColumn.HeaderText = "Fecha";
-            this.fechaDataGridViewTextBoxColumn.Name = "fechaDataGridViewTextBoxColumn";
-            // 
-            // EnEdicionBindingSource
-            // 
-            this.EnEdicionBindingSource.DataSource = typeof(ENTIDADES.CreditosDebitos);
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.groupBox2.Controls.Add(this.FiltradoMotivoComboBox);
-            this.groupBox2.Controls.Add(this.FiltradoMotivoslabel);
-            this.groupBox2.Location = new System.Drawing.Point(38, 390);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(337, 51);
-            this.groupBox2.TabIndex = 15;
-            this.groupBox2.TabStop = false;
-            // 
-            // Form1
+            // AsientoContable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1060, 450);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Salirbutton);
             this.Controls.Add(this.Emitirbutton);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.NotaExplicativaLabel);
+            this.Controls.Add(this.btnGenerarInformeDetallado);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.MotivoAjusteComboBox);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.motivoajusteLabel);
-            this.Name = "Form1";
-            this.Text = "Asistente de Créditos y Débitos";
+            this.Name = "AsientoContable";
+            this.Text = "Asiento contable";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.AsientoEnEdicionBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.CreditosyDebitosBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.EnEdicionBindingSource)).EndInit();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AsientoContableBindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label motivoajusteLabel;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.ComboBox MotivoAjusteComboBox;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label FiltradofechasLabel;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.Label FiltradoMotivoslabel;
-        private System.Windows.Forms.ComboBox FiltradoMotivoComboBox;
-        private System.Windows.Forms.Label NotaExplicativaLabel;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGenerarInformeDetallado;
         private System.Windows.Forms.Button Emitirbutton;
         private System.Windows.Forms.Button Salirbutton;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.BindingSource CreditosyDebitosBindingSource;
-        private System.Windows.Forms.DataGridViewTextBoxColumn motivoDelAjusteDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn notaExplicativaDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn creditoDebitoDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn fechaDataGridViewTextBoxColumn;
-        private System.Windows.Forms.BindingSource EnEdicionBindingSource;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.BindingSource AsientoContableBindingSource;
+        private System.Windows.Forms.BindingSource AsientoEnEdicionBindingSource;
     }
 }
 
--- FACTURACION/Form1.cs
+++ FACTURACION/Form1.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace FACTURACION
 {
-    public partial class Form1 : Form
+    public partial class AsientoContable : Form
     {
-        public Form1()
+        public AsientoContable()
         {
             InitializeComponent();
         }
@@ -21,5 +19,114 @@ namespace FACTURACION
         {
 
         }
+
+        private void Salirbutton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Emitirbutton_Click(object sender, EventArgs e)
+        {
+        }
+
+        public class ExcelProcessor
+        {
+            public void ProcessExcelFile(string filePath)
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filePath);
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                // Validar la fecha del asiento
+                DateTime date;
+                if (!DateTime.TryParse(xlRange.Cells[1, 2].Value2.ToString(), out date))
+                {
+                    MessageBox.Show("Formato de fecha incorrecto en la celda B1");
+                    return;
+                }
+
+                // Validar el encabezado del asiento
+                string header = xlRange.Cells[2, 2].Value2.ToString();
+                if (string.IsNullOrEmpty(header))
+                {
+                    MessageBox.Show("Falta el encabezado del asiento en la celda B2");
+                    return;
+                }
+
+                // Procesar las filas a partir de la fila 5
+                for (int i = 5; i <= xlRange.Rows.Count; i++)
+                {
+                    string accountCode = xlRange.Cells[i, 1].Value2.ToString();
+                    string accountDescription = xlRange.Cells[i, 2].Value2.ToString();
+                    decimal debitAmount = Convert.ToDecimal(xlRange.Cells[i, 3].Value2);
+                    decimal creditAmount = Convert.ToDecimal(xlRange.Cells[i, 4].Value2);
+
+                    // Crear el asiento contable
+                    CreateAccountingEntry(accountCode, accountDescription, debitAmount, creditAmount);
+                }
+
+                MessageBox.Show("Asiento contable creado exitosamente");
+
+                // Limpiar
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+            }
+            private void CreateAccountingEntry(string accountCode, string accountDescription, decimal debitAmount, decimal creditAmount)
+            {
+                public List<Asiento> Asientos { get; set; }
+
+            public SistemaContable()
+            {
+                Asientos = new List<Asiento>();
+            }
+
+            public bool CrearAsientoDesdeExcel(string rutaArchivo)
+            {
+                if (!ValidarFormato(rutaArchivo))
+                {
+                    return false;
+                }
+
+                using (var paquete = new ExcelPackage(new FileInfo(rutaArchivo)))
+                {
+                    var hoja = paquete.Workbook.Worksheets[0];
+                    var asiento = new Asiento
+                    {
+                        // Aquí debes ajustar los índices de las celdas según tu archivo Excel
+                        Propiedad1 = hoja.Cells[1, 1].Value.ToString(),
+                        Propiedad2 = hoja.Cells[1, 2].Value.ToString(),
+                        // Agrega todas las propiedades necesarias
+                    };
+
+                    Asientos.Add(asiento);
+                }
+
+                return true;
+            }
+
+            public bool ValidarFormato(string rutaArchivo)
+            {
+                // Aquí debes implementar la lógica para validar el formato del archivo Excel
+                throw new NotImplementedException();
+            }
+        }
+
+        public class Asiento
+        {
+            public string Propiedad1 { get; set; }
+            public string Propiedad2 { get; set; }
+            // Agrega todas las propiedades necesarias
+        }
+    }
     }
 }
